d34a382a7680ab1fa4aa010832fe4131b89483a0
ThanGrove <than@virginia.edu>
2013-12-18 13:47:20 -0500

    Updating page template to put highlight region under breadcrumbs and header

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -147,7 +147,6 @@ global $user;
       </div></div><!-- /.section, /#navigation -->
     <?php endif; ?>
     <div id="content" class="column"><div class="section">
-      <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
@@ -155,6 +154,7 @@ global $user;
         <h1 class="title" id="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
+      <?php print render($page['highlighted']); ?>
       <?php print $messages; ?>
       <?php if ($tabs = render($tabs)): ?>
         <div class="tabs"><?php print $tabs; ?></div>
